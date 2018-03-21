872b09a045f49dca6142e9bafcdad3b3504f37e9
rhrlima <ricardo_des@hotmail.com>
2018-03-21 15:30:21 -0300

    Adding evolutive examples (GA and DE)

--- ec/examples/tsp/TSP.java
+++ ec/examples/tsp/TSP.java
@@ -12,9 +12,9 @@ public class TSP extends Problem implements SimpleProblemForm {
 
 	public static final String P_INSTANCE_FILE = "instance";
 	public Parameter defaultBase() {return new Parameter(P_INSTANCE_FILE);}
-        
-        private int [][]distances;
-        
+		
+		private int [][]distances;
+		
 	public void setup(final EvolutionState state, final Parameter base)
 	{
 		super.setup(state, base);
@@ -22,8 +22,8 @@ public class TSP extends Problem implements SimpleProblemForm {
 		Parameter def = defaultBase();
 		InputStream stream = state.parameters.getResource(base.push(P_INSTANCE_FILE), 
 			def.push(P_INSTANCE_FILE));
-                
-                readInstance(stream, state);
+				
+				readInstance(stream, state);
 	}
 
 	public void evaluate(final EvolutionState state, 
@@ -50,18 +50,18 @@ public class TSP extends Problem implements SimpleProblemForm {
 			sum == ind2.genome.length); //is it ideal?
 		ind2.evaluated = true;
 	}
-        
-        private void readInstance(InputStream stream, EvolutionState state) {
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
-                
-                while(br.ready()) {
-                    String temp = br.readLine();
-                    state.output.warning(temp);
-                }
-                
-                br.close();
-            } catch (Exception e) {
-                state.output.fatal("Falha ao tentar abrir o arquivo de instância do problema.", null);
-            }
-        }
+		
+	private void readInstance(InputStream stream, EvolutionState state) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
+			
+			while(br.ready()) {
+				String temp = br.readLine();
+				state.output.warning(temp);
+			}
+			
+			br.close();
+		} catch (Exception e) {
+			state.output.fatal("Falha ao tentar abrir o arquivo de instância do problema.", null);
+		}
+	}
 }
